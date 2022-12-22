4510aa1581d8e01fec1d4f924f4d335544a52541
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-22 17:09:05 +0100

    created next carousel

--- js/script.js
+++ js/script.js
@@ -25,36 +25,51 @@ const gallery = document.getElementById('gallery');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
+const images = document.querySelectorAll('#gallery img')
+
+let currentActiveIndex = 0;
+images[currentActiveIndex].classList.add ('active');
+
 // Array di oggetti
 const data = [
   {
-    image: 'img/01.webp',
+    image: 'img/0.webp',
     title: 'Marvel\'s Spiderman Miles Morale',
     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
   }, {
-    image: 'img/02.webp',
+    image: 'img/1.webp',
     title: 'Ratchet & Clank: Rift Apart',
     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
   }, {
-    image: 'img/03.webp',
+    image: 'img/2.webp',
     title: 'Fortnite',
     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
   }, {
-    image: 'img/04.webp',
+    image: 'img/3.webp',
     title: 'Stray',
     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
   }, {
-    image: 'img/05.webp',
+    image: 'img/4.webp',
     title: "Marvel's Avengers",
     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
   }
 ];
 
-for (let i = 0; i < data.length; i++) {
-    let singleCard =  data[i];
 
-    const signleCarElement = `
-    <img class ="active" src="img/${i}.webp" alt="">
-    `
-    gallery.innerHTML += signleCarElement;
-}
+next.addEventListener ('click', function(){
+
+    // Rimuovo la classe active 
+    images[currentActiveIndex].classList.remove ('active');
+
+    // Incremento per fargli cambiare immagine
+    currentActiveIndex++
+
+    if (currentActiveIndex === data.length) {
+        currentActiveIndex = 0;
+    }
+
+    // Assegno la classe active alla foto successiva
+    images[currentActiveIndex].classList.add ('active');
+
+})
+
